f956d646e918b6de042239627d985e06a39df80d
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-02-24 16:58:13 +0100

    amation and page fotografia slubna

--- app/fotografia-slubna/page.tsx
+++ app/fotografia-slubna/page.tsx
@@ -1,10 +1,52 @@
+"use client";
+import { useEffect, useState } from "react";
+import { SlArrowDown } from "react-icons/sl";
 export default function FotografiaSlubna() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
-        <div className="text-black py-6 px-3 mt-10">
+        <>
             <div className="text-center">
-                <p>Slub</p>
-                <div style={{backgroundImage: "url(https://scontent-waw2-2.cdninstagram.com/v/t51.29350-15/356403991_795416282133770_1453407733380961786_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE0NDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-waw2-2.cdninstagram.com&_nc_cat=102&_nc_oc=Q6cZ2AGewFLT2FJ6OGmleAJWPcwAFTvLa6wHYqnJwz5c1Jrk5FD-DosTHH47ar_uy1rjw-U&_nc_ohc=PHNsweUWPKQQ7kNvgEagx6o&_nc_gid=65eff408429949cd854c9ad396a1e4fb&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzEzNTIyNTkyODExMDY0MzgxMQ%3D%3D.3-ccb7-5&oh=00_AYAFKYkSoDK9avXDuxWTYKhTiTYcFeZB6zqtISGMOHObZA&oe=67C26224&_nc_sid=7a9f4b)"}}></div>
+                <div className="pt-[40vh] pb-[20vh] text-white">
+                    <p className=" text-3xl font-semibold">Slub</p>
+                    <p className="px-6 max-w-screen-sm mx-auto">
+                        Dzień, który zapamiętacie na zawsze, ten wymarzony,
+                        najpiękniejszy, idealny. Czemu chcę być z Wami? Żeby
+                        łapać te wszystkie piękne, ale jednocześnie ulotne
+                        chwile i zachowywać je na zdjęciach.
+                    </p>
+                </div>
+                <SlArrowDown
+                    className={`text-4xl text-white/80 mx-auto mb-2 transition-all duration-150 ${
+                        isScrolled ? "translate-y-4 opacity-0" : "opacity-100"
+                    }`}
+                />
+                <div
+                    className="h-screen fixed top-0 left-0 w-full bg-cover bg-center bg-no-repeat -z-40"
+                    style={{
+                        backgroundImage: "url('/Images/sesja.jpg')",
+                    }}
+                />
+                <div className="bg-black/40 absolute top-0 left-0 w-full h-screen -z-30" />
+                <div className="bg-white w-full py-48">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Distinctio illo magni magnam?
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
