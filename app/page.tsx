67814f6be1e4c42313204094784f566bfb190d73
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-02-25 18:03:27 +0100

    dgf

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Jarek from "@/public/Images/fotograf-siedlce-jarek-olszewski.jpg";
 import Slub from "@/public/Images/slub.jpg";
 import Link from "next/link";
 import CTA from "@/components/CTA";
+import SesjaComponent from "@/components/SesjaComponent";
 
 export default function Home() {
     return (
@@ -19,7 +20,7 @@ export default function Home() {
                         height={300}
                         alt="fotograf siedlce jarek-olszewski"
                     />
-                    <h2 className="w-[340px] lg:w-[400px] bg-black/80 lg:-ml-20 lg:mt-0 -mt-12 px-12 py-6 text-white text-xs tracking-tight backdrop-blur-md">
+                    <h2 className="w-[340px] lg:w-[400px] bg-black/60 lg:-ml-20 lg:mt-0 -mt-12 px-12 py-6 text-white text-xs tracking-tight backdrop-blur-sm">
                         <strong className="text-2xl font-semibold">
                             Nazywam się Jarek i jestem fotografem
                         </strong>{" "}
@@ -41,26 +42,34 @@ export default function Home() {
                     doświadczenia i wyjątkowych zdjęć. moje fotografie staram
                     się by były naturalne i prawdziwe, a przez to ponadczasowe…
                 </p>
-                <div className="grid grid-cols-1 py-12 lg:grid-cols-4 max-w-screen-md mx-auto h-full w-full">
+                <div className="relative w-[80%] lg:mr-24 md:w-1/2 md:h-80 lg:h-96 md:ml-auto ml-12 h-48 mb-24 px-3 -space-x-10 lg:space-x-0 mt-12">
                     <Image
                         src={Slub}
-                        height={500}
-                        width={600}
                         alt="slub"
-                        className="object-cover lg:order-1 col-span-3"
+                        fill
+                        className="object-cover"
+                        quality={75}
                     />
-                    <div className="bg-black/80 text-start p-12 lg:p-12 -mt-12 mx-12 lg:mt-12 lg:-ml-12 lg:order-2 z-10 h-max w-max">
-                        <p className="capitalize text-white text-5xl mb-8">
-                            reportaż slubny
+
+                    <div className="absolute -bottom-12 md:bottom-1/2 md:translate-y-1/2 md:-left-1/3 md:-translate-x-1/2 left-0 bg-black/60 text-start px-8 py-6 backdrop-blur-sm lg:p-12 z-10 h-max w-full">
+                        <p className="capitalize text-white text-2xl lg:text-5xl mb-2">
+                            reportaż ślubny
                         </p>
                         <Link
                             href="/"
-                            className="text-white py-4 px-8 border border-white"
+                            className="text-white text-sm py-1 px-4 border border-white"
                         >
                             Zobacz
                         </Link>
                     </div>
                 </div>
+
+                <section className="grid grid-cols-1 lg:grid-cols-3">
+                    <SesjaComponent img={Slub} title="Ślub" href="/" />
+                    <SesjaComponent img={Slub} title="Sesja" href="/" />
+                    <SesjaComponent img={Slub} title="C" href="/" />
+                </section>
+
                 <div className="space-y-4 p-12">
                     <h3 className="text-2xl">
                         Porozmawiajmy o Twoim pomyśle na zdjęcia{" "}
---
+++ components/SesjaComponent.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface SesjaComponentProps {
+    img: Image;
+    href: string;
+    title: string;
+}
+
+export default function SesjaComponent({
+    img,
+    href,
+    title,
+}: SesjaComponentProps) {
+    return (
+        <div className="relative w-[80%] ml-12 h-48 mb-24 px-3 -space-x-10 mt-12">
+            <Image
+                src={img}
+                alt={title}
+                fill
+                className="object-cover"
+                quality={75}
+            />
+            <div className="absolute -bottom-12 left-0 bg-black/60 text-start px-8 py-6 backdrop-blur-sm  z-10 h-max w-full">
+                <p className="capitalize text-white text-2xl mb-2">
+                    {title}
+                </p>
+                <Link
+                    href={href}
+                    className="text-white text-sm py-1 px-4 border border-white"
+                >
+                    Zobacz
+                </Link>
+            </div>
+        </div>
+    );
+}
