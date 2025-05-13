3ebcfe3a595bfa8c8cadbf1974ddd3d6f53dac68
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-05-13 12:38:37 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,30 @@ export const metadata: Metadata = {
     title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
     description:
         "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
+    openGraph: {
+        title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
+        description:
+            "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
+        url: "https://jv3.vercel.app/",
+        siteName: "Jarek Olszewski - Fotograf",
+        images: [
+            {
+                url: "/Images/fotograf-siedlce-jarek-olszewski.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Fotografia ślubna - Jarek Olszewski",
+            },
+        ],
+        locale: "pl_PL",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
+        description:
+            "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
+        images: ["/Images/fotograf-siedlce-jarek-olszewski.jpg"],
+    },
 };
 
 export default function RootLayout({
