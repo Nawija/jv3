afcb6b31161d1804579341b3741fbff9edadd490
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-05-14 12:53:21 +0200

    Update page.tsx

--- app/oferta/page.tsx
+++ app/oferta/page.tsx
@@ -48,7 +48,7 @@ export default async function OffertsPage() {
     return (
         <>
             <div className="bg-stone-50 py-8 px-6 sm:px-6 lg:px-8">
-                <section className="max-w-screen-md mx-auto space-y-8 anim-opacity text-stone-800">
+                <section className="max-w-7xl mx-auto space-y-8 anim-opacity text-stone-800">
                     <h1 className="text-4xl text-center leading-tight">
                         Profesjonalny Fotograf Siedlce
                     </h1>
@@ -64,11 +64,11 @@ export default async function OffertsPage() {
                             Dlaczego warto wybrać mnie?
                         </h2>
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6">
                             {benefits.map(([title, desc], i) => (
                                 <div
                                     key={i}
-                                    className="flex gap-4 items-start p-4 bg-white rounded-xl shadow-sm hover:shadow-md transition"
+                                    className="flex gap-4 items-start p-4 bg-white rounded-xl shadow-sm"
                                 >
                                     <FaCheckCircle className="text-main mt-1 text-xl flex-shrink-0" />
                                     <div>
