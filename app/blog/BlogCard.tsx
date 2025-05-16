9fb2d2dcb502cd5b848e6522ba137408e7f02076
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-17 00:58:56 +0200

    Update BlogCard.tsx

--- app/blog/BlogCard.tsx
+++ app/blog/BlogCard.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 type Props = {
     title: string;
@@ -11,26 +11,24 @@ type Props = {
 };
 
 const InfoBlock: FC<Props> = ({ title, image, slug }) => {
-    const [loaded, setLoaded] = useState(false);
+    // const [loaded, setLoaded] = useState(false);
 
     return (
         <Link
             href={`/blog/${slug}`}
             className="text-center border p-2 border-gray-300 block relative overflow-hidden"
         >
-            {!loaded && (
+            {/* {!loaded && (
                 <div className="absolute inset-0 animate-pulse bg-gray-300 z-0" />
-            )}
+            )} */}
 
             <Image
                 src={image}
                 alt={title}
                 width={300}
                 height={300}
-                onLoad={() => setLoaded(true)}
-                className={`w-full h-auto object-cover transition-opacity duration-500 ${
-                    loaded ? "opacity-100" : "opacity-0"
-                }`}
+                // onLoad={() => setLoaded(true)}
+                className={`w-full h-auto object-cover transition-opacity duration-500 `}
             />
 
             <h2 className="font-medium my-2 text-xl relative z-10">{title}</h2>
