35fe326e2c2cc706be2db84af28a234fa3e800d9
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-17 01:52:21 +0200

    df

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Header/Nav";
@@ -10,35 +10,21 @@ const geistSans = Geist({
     subsets: ["latin"],
 });
 
-// export const metadata: Metadata = {
-//     title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
-//     description:
-//         "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
-//     openGraph: {
-//         title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
-//         description:
-//             "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
-//         url: "https://jv3.vercel.app/",
-//         siteName: "Jarek Olszewski - Fotograf",
-//         images: [
-//             {
-//                 url: "/Images/fotograf-siedlce-jarek-olszewski.jpg",
-//                 width: 1200,
-//                 height: 630,
-//                 alt: "Fotografia ślubna - Jarek Olszewski",
-//             },
-//         ],
-//         locale: "pl_PL",
-//         type: "website",
-//     },
-//     twitter: {
-//         card: "summary_large_image",
-//         title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
-//         description:
-//             "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
-//         images: ["/Images/fotograf-siedlce-jarek-olszewski.jpg"],
-//     },
-// };
+export const metadata: Metadata = {
+    openGraph: {
+        url: "https://jv3.vercel.app/",
+        siteName: "Jarek Olszewski - Fotograf",
+        locale: "pl_PL",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Fotograf Siedlce: Fotografia Ślubna i Artystyczna, Foto",
+        description:
+            "Fotograf Siedlce: Jarek Olszewski fotografia ślubna i reportaże. Najlepszy fotograf ślubny w Siedlcach. Zobacz portfolio i zamów sesję!",
+        images: ["/Images/fotograf-siedlce-jarek-olszewski.jpg"],
+    },
+};
 
 export default function RootLayout({
     children,
