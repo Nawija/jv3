b29279ee0a85664e306ca601f25860ba5aed5c8f
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-20 23:38:52 +0200

    Update InstagramPost.tsx

--- components/instagram/InstagramPost.tsx
+++ components/instagram/InstagramPost.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import ShimmerLoader from "../ShimmerLoader";
 
@@ -16,8 +12,6 @@ export default function InstagramPost({
     permalink,
     alt,
 }: InstagramPostProps) {
-    const [loading, setLoading] = useState(true);
-
     if (!media_url || !permalink) {
         return (
             <div className="aspect-square">
@@ -29,17 +23,11 @@ export default function InstagramPost({
     return (
         <Link href={permalink} target="_blank" rel="noopener noreferrer">
             <div className="relative aspect-square">
-                {loading && (
-                    <div className="absolute inset-0 z-10">
-                        <ShimmerLoader />
-                    </div>
-                )}
                 <img
                     src={media_url}
                     alt={alt}
                     sizes="20vw"
                     className={`object-cover h-full w-full`}
-
                 />
             </div>
         </Link>
