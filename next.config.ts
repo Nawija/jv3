efd8fec22a425bb6e576e70706a78a1a6b1eac15
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-20 23:59:08 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,6 @@ const nextConfig: NextConfig = {
     images: {
         deviceSizes: [640, 768],
         domains: ["*.cdninstagram.com"],
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "scontent-waw2-2.cdninstagram.com",
-            },
-            {
-                protocol: "https",
-                hostname: "scontent-waw2-1.cdninstagram.com",
-            },
-            {
-                protocol: "https",
-                hostname: "scontent-waw1-1.cdninstagram.com",
-            },
-            {
-                protocol: "https",
-                hostname: "scontent-iad3-1.cdninstagram.com",
-            },
-        ],
     },
 };
 
