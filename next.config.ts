6ddd813fac29c39100820c6233dc22e45b734c5a
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-20 23:28:50 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
         deviceSizes: [640, 768, 1024, 1280, 1600],
-        imageSizes: [16, 32, 48, 64, 96],
-        formats: ["image/avif", "image/webp"],
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "scontent-waw2-2.cdninstagram.com",
-                port: "", // lub pomiń
-                pathname: "/**",
             },
             {
                 protocol: "https",
                 hostname: "scontent-waw2-1.cdninstagram.com",
-                port: "", // lub pomiń
-                pathname: "/**",
             },
             {
                 protocol: "https",
                 hostname: "scontent-waw1-1.cdninstagram.com",
-                port: "",
-                pathname: "/**",
             },
         ],
     },
