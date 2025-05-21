b8dc77cc46d9558aaf0b18f9097e5c4bb7fa041b
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-05-21 15:16:31 +0200

    Update MainBtn.tsx

--- components/Buttons/MainBtn.tsx
+++ components/Buttons/MainBtn.tsx
@@ -9,7 +9,7 @@ export function MainBtn({ children, className, ...rest }: ButtonProps) {
         <button
             {...rest}
             className={clsx(
-                "relative font-normal bg-neutral-900 hover:bg-stone-700 transition-colors px-4 py-2 text-sm lg:text-base text-white uppercase",
+                "relative font-normal bg-neutral-900 hover:bg-stone-700 transition-colors px-4 py-2 text-sm lg:text-base text-white uppercase border",
                 className
             )}
         >
