ea0b32267ccabbfffa2375d017d8aea9c5c7ecd3
Valkon <96625039+Nawija@users.noreply.github.com>
2025-05-24 22:10:35 +0200

    df

--- constants/Links.ts
+++ constants/Links.ts
@@ -169,7 +169,7 @@ export const OFERTAIMAGES = [
         desc: "Od przygotowań po wesele - wszystkie wydarzenie z tego dnia",
     },
     {
-        src: "/Images/oferta/SESJA NARZECZEŃSKA.avif",
+        src: "/Images/oferta/SESJA-NARZECZENSKA.avif",
         position: "55% 0%",
         href: "/sesja-narzeczenska",
         title: "Sesja Narzeczeńska",
